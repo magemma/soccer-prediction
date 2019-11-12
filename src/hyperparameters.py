501481d70f9a9bff5e662de6b256abec8322aab3
Mohammad Razzaghnoori <razzaghnoori@gmail.com>
2019-11-12 18:39:32 +0100

    Ask user which target they want and only run models suited for that specific target

--- src/hyperparameters.py
+++ src/hyperparameters.py
@@ -1,5 +1,25 @@
 params = {
-    'svc': [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1000]},
-            {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}],
-    
+    'mlp': {
+        'type': 'categorical',
+        'params': [
+            {
+                'activation': ['relu', 'logistic', 'tanh'], 
+                'hidden_layer_sizes': [(5,2), (10,5)]
+            }
+        ],
+    },
+    'svc': {
+        'type': 'categorical',
+        'params': [
+            {
+                'kernel': ['rbf'], 
+                'gamma': [1e-3, 1e-4], 
+                'C': [1, 10, 100, 1000]
+            },
+            {
+                'kernel': ['linear'], 
+                'C': [1, 10, 100, 1000]
+            }
+        ],
+    }
 }
